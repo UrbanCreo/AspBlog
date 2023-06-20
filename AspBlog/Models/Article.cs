ce99e665b419ec67c16a1279c618ba58ac5864af
TomasUrban <129217734+UrbanCreo@users.noreply.github.com>
2023-06-20 18:54:21 +0200

    Files upload

---
+++ AspBlog/Models/Article.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspBlog.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Vyplňte obsah")]
+
+        [DisplayName("Kontent")]
+        public string Content { get; set; } = "";
+        [Required(ErrorMessage = "Vyplňte titulek")]
+        [StringLength(100, ErrorMessage = "Titulek je příliš dlouhý")]
+
+        [DisplayName("Titulek")]
+        public string Title { get; set; } = "";
+        [Required(ErrorMessage = "Vyplňte popisek")]
+
+        [DisplayName("Popis")]
+        public string Description { get; set; } = "";
+    }
+}
